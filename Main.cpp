e11e3dfb52a2a16e06a8a1aa3a6f7fec141e0789
apemanjosh67 <joshmuszka67@gmail.com>
2023-01-19 15:19:56 -0500

    setting some stuff up

--- Main.cpp
+++ Main.cpp
@@ -1,37 +1,50 @@
 #include <iostream>
-#include </home/apeman/Code/Test/GAME_PROJECT/Libraries/include/glad/glad.h>
+//#include </home/apeman/Code/Test/GAME_PROJECT/Libraries/include/glad/glad.h>
+#include </home/apeman/code/VSCode/GAME_PROJECT/Libraries/include/glad/glad.h>
 //#include <GLES2/gl2.h>
 //#include <EGL/egl.h>
 #include <GLFW/glfw3.h>
 
 int main() 
 {
-	//Initilizes
-	glfwInit();
+	bool initialized = glfwInit();
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Programming Project", NULL, NULL);
-
-	if (window == NULL)
+	if (!initialized)
 	{
-		std::cout << "The window failed to be created" << std::endl;
-		glfwTerminate();
-		return -1;
+		//initialization didnt work
+	}
+
+	//TODO: event error callback
+
+	//initialize window, create context
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Game window", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	gladLoadGL(); //needed for glad avoid segmentation fault blah blah blah
+
+	//TODO: gladLoadGL(glfwGetProcAddress);
+
+	//TODO: constrain max and min opengl versions
+	if (!window)
+	{
+		//window context creation failed
 	}
 
 	
 
-	glfwMakeContextCurrent(window);
-	
+	//MAIN LOOP
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwPollEvents();
+		glad_glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window); //swap front and back buffers	
+		glfwPollEvents(); //this gets the window to be closeable
 	}
 
+	//TODO: input events
+
+	//destroy window context
 	glfwDestroyWindow(window);
+
+	//end application
 	glfwTerminate();
 	return 0;
 }
