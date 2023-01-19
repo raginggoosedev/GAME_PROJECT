1f0655e217a09f1d3ac55f16faecaa7cd7199c99
Knightlynick <nwoo68@gmail.com>
2023-01-18 20:37:41 -0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() 
+{
+	//Initilizes
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Programming Project", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "The window failed to be created" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
