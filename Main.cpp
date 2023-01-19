c29e70769eb767453061ee5546b27305eba04d8b
ExplorerDonutz <mwquick04@gmail.com>
2023-01-18 22:01:21 -0500

    Basic input & window

--- Main.cpp
+++ Main.cpp
@@ -2,37 +2,53 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(void) 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	// Resize viewport when window is resized
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
 {
-	//Initilizes
-	if (!glfwInit()) {
-		exit(EXIT_FAILURE);
-	}
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+int main() 
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Programming Project", NULL, NULL);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Programming Project", NULL, NULL);
-
-	if (!window)
+	if (window == NULL)
 	{
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-		exit(EXIT_FAILURE);
+		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		processInput(window);
 
-		//glClear(GL_COLOR_BUFFER_BIT);
-
-		//glfwSwapBuffers(window);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	glfwDestroyWindow(window);
 	glfwTerminate();
-	exit(EXIT_SUCCESS);
+	return 0;
 }
