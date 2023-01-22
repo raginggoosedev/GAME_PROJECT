77bee4c294b81976a951013f73ef2026d6e205dd
Knightlynick <nwoo68@gmail.com>
2023-01-21 21:55:01 -0500

    Place Holder in Case anything messes up

--- Player.cpp
+++ Player.cpp
@@ -4,25 +4,18 @@
 #include "ShaderVariables.cpp"
 #define GLFW_INCLUDE_NONE
 
-
-
-class Player
+static const struct
 {
+	float x, y;
+	float r, g, b;
+} vertices[3] =
+{
+			{ -0.6f, -0.4f, 1.f, 0.f, 0.f },
+			{  0.6f, -0.4f, 0.f, 1.f, 0.f },
+			{   0.0f,  0.6f, 0.f, 0.f, 1.f }
+};
 
-private:
-	GLuint vertexBuffer, vertexShader, fragmentShader;
-	GLint mvpLocation, vposLocation, vcolLocation;
-	ShaderVariables* variables;
-
-public:
-	GLuint program;
-
-	Player()
-	{
-		variables = new ShaderVariables();
-
-		//Initilize Variable
-		variables->vertex_shader_text = 
+static const char* vertex_shader_text =
 		"#version 110\n"
 		"uniform mat4 MVP;\n"
 		"attribute vec3 vCol;\n"
@@ -32,36 +25,41 @@ public:
 		"{\n"
 		"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
 		"    color = vCol;\n"
-			"}\n";
+		"}\n";
+static const char* fragment_shader_text =
+		"#version 110\n"
+		"varying vec3 color;\n"
+		"void main()\n"
+		"{\n"
+		"    gl_FragColor = vec4(color, 1.0);\n"
+		"}\n";
 
-		variables->vertex_shader_text =
-			"#version 110\n"
-			"varying vec3 color;\n"
-			"void main()\n"
-			"{\n"
-			"    gl_FragColor = vec4(color, 1.0);\n"
-			"}\n";
+class Player
+{
 
-		variables->vertices =
-		{
-			{ -0.6f, -0.4f, 1.f, 0.f, 0.f },
-			{  0.6f, -0.4f, 0.f, 1.f, 0.f },
-			{   0.0f,  0.6f, 0.f, 0.f, 1.f }
-		};
+private:
+	GLuint vertexBuffer, vertexShader, fragmentShader;
+	GLint mvpLocation, vposLocation, vcolLocation;
+
+public:
+	GLuint program;
+
+	Player()
+	{
 
 		//Setting up the shaders
 		glGenBuffers(1, &vertexBuffer);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexBuffer), variables->vertices, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexBuffer), vertices, GL_STATIC_DRAW);
 
 		//VERTEX SHADERS
 		vertexShader = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertexShader, 1, &variables->vertex_shader_text, NULL);
+		glShaderSource(vertexShader, 1, &vertex_shader_text, NULL);
 		glCompileShader(vertexShader);
 
 		//FRAGMENT SHADERS
 		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragmentShader, 1, &variables->fragment_shader_text, NULL);
+		glShaderSource(fragmentShader, 1, &fragment_shader_text, NULL);
 		glCompileShader(fragmentShader);
 
 		//PROGRAM
@@ -76,10 +74,10 @@ public:
 
 		//Enable the buffers
 		glEnableVertexAttribArray(vposLocation);
-		glVertexAttribPointer(vposLocation, 2, GL_FLOAT, GL_FALSE, sizeof(variables->vertices[0]), (void*)0);
+		glVertexAttribPointer(vposLocation, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void*)0);
 
 		glEnableVertexAttribArray(vcolLocation);
-		glVertexAttribPointer(vcolLocation, 3, GL_FLOAT, GL_FALSE, sizeof(variables->vertices[0]), (void*)(sizeof(float) * 2));
+		glVertexAttribPointer(vcolLocation, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void*)(sizeof(float) * 2));
 
 		//Draw the triangle
 		glDrawArrays(GL_TRIANGLES, 0, 3);
--- ShaderVariables.cpp
+++ ShaderVariables.cpp
@@ -1,16 +1,11 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#define GLFW_INCLUDE_NONE
 
 class ShaderVariables
 {
 public:
 	static const char* vertex_shader_text;
 	static const char* fragment_shader_text;
-
-	static const struct
-	{
-		float x, y;
-		float r, g, b;
-	} vertices[3];
 };
